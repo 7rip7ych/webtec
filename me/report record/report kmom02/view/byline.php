97a0a27ace2dbfc9b4af21b055a471dffb2533dd
Ida Alin <dev.7rip7ych@gmail.com>
2025-06-19 14:34:10 +0200

    Removed som files

--- me/report record/report kmom02/view/byline.php
+++
@@ -1,8 +0,0 @@
-<footer class="byline">
-    <figure class="figure left">
-        <img src="img/7rip7ych.png" alt="7rip7ych" class="byline-img">
-        <figcaption>7rip7ych</figcaption>
-      </figure>
-      <p class="byline-text"><span class="first-word">7rip7ych</span> är en junior programmerare som för tillfället studerar på BTH.
-       De har erfarenhet av programmering i HTML, CSS, Javascript, Python och C#. Även viss erfarenhet i bild- och videoredigering, design och musikproduktion.</p>
-</footer>
